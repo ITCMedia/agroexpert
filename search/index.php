f3b9a257e9dd4c852a46623e39b600a3221d1c0d
itcmedia <support@itc-media.ru>
2019-09-17 17:39:09 +0300

    Add files via upload

--- search/index.php
+++ search/index.php
@@ -1,1 +1,148 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Поиск</title>
 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="../css/style.css" rel="stylesheet"/>
+    <link href="../css/font-awesome/css/all.min.css" rel="stylesheet">
+    <link href="../css/font-awesome/css/fontawesome.min.css" rel="stylesheet">
+    <style>
+        .top-menu__search {
+            display: none;
+        }
+    </style>
+</head>
+<body>
+<? require (__DIR__  . '/../header.php'); ?>
+
+    <section class="breadcrumb">
+        <div class="container">
+            <ul itemscope itemtype="http://schema.org/BreadcrumbList">
+                <li class="breadcrumb-item" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+                    <a itemscope itemtype="http://schema.org/Thing" itemprop="item" href="">
+                        <span itemprop="name">Главная</span>
+                    </a>
+                    <meta itemprop="position" content="1"/>
+                </li>
+                <li class="breadcrumb-item" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+                    <span itemprop="name">Поиск</span>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="content">
+        <div class="container">
+            <div class="row mb-4">
+                <p class="search__block">
+                    <i class="fas fa-search"></i>
+                    <input class="input-row__search search" value="Сытный" placeholder="Поиск" />
+                </p>
+            </div>
+            <div class="search__title row">
+                <h1>Результаты поиска <span class="result-count">20</span></h1>
+            </div>
+            <div class="result-list">
+                <div class="result-item row">
+                    <div class="col-1"></div>
+                    <div class="col-11">
+                        <div class="search-result__title">В 2017 году зарегистрированы ТМ «<font>Сытный</font>» и ТМ «<font>Сытный</font> ПРО». С 2019 года разработаны и выведены на рынок корма ТМ «КОРМЛИНА».</div>
+                        <div class="search-result__description">В производстве используется только качественное сырье. Часть зерна выращивается по техническому заданию нашей производственной лаборатории сельскохозяйственными производителями, практикующими безопасное земледелие по системе «Био».</div>
+                        <div class="search-item__section">Новости</div>
+                    </div>
+                </div>
+                <div class="result-item row">
+                    <div class="col-1"></div>
+                    <div class="col-11">
+                        <div class="search-result__title"><font>Сытный</font>™ PRO Телёнок рост</div>
+                        <div class="search-result__description"></div>
+                        <div class="search-item__section">Каталог товаров</div>
+                    </div>
+                </div>
+                <div class="result-item row">
+                    <div class="col-1"></div>
+                    <div class="col-11">
+                        <div class="search-result__title"><font>Сытный</font>™ PRO Телёнок рост</div>
+                        <div class="search-result__description"></div>
+                        <div class="search-item__section">Каталог товаров</div>
+                    </div>
+                </div>
+                <div class="result-item row">
+                    <div class="col-1"></div>
+                    <div class="col-11">
+                        <div class="search-result__title">В 2017 году зарегистрированы ТМ «<font>Сытный</font>» и ТМ «<font>Сытный</font> ПРО». С 2019 года разработаны и выведены на рынок корма ТМ «КОРМЛИНА».</div>
+                        <div class="search-result__description">В производстве используется только качественное сырье. Часть зерна выращивается по техническому заданию нашей производственной лаборатории сельскохозяйственными производителями, практикующими безопасное земледелие по системе «Био».</div>
+                        <div class="search-item__section">Новости</div>
+                    </div>
+                </div>
+                <div class="result-item row">
+                    <div class="col-1"></div>
+                    <div class="col-11">
+                        <div class="search-result__title"><font>Сытный</font>™ PRO Телёнок рост</div>
+                        <div class="search-result__description"></div>
+                        <div class="search-item__section">Каталог товаров</div>
+                    </div>
+                </div>
+                <div class="result-item row">
+                    <div class="col-1"></div>
+                    <div class="col-11">
+                        <div class="search-result__title"><font>Сытный</font>™ PRO Телёнок рост</div>
+                        <div class="search-result__description"></div>
+                        <div class="search-item__section">Каталог товаров</div>
+                    </div>
+                </div>
+                <div class="result-item row">
+                    <div class="col-1"></div>
+                    <div class="col-11">
+                        <div class="search-result__title"><font>Сытный</font>™ PRO Телёнок рост</div>
+                        <div class="search-result__description"></div>
+                        <div class="search-item__section">Каталог товаров</div>
+                    </div>
+                </div>
+                <div class="result-item row">
+                    <div class="col-1"></div>
+                    <div class="col-11">
+                        <div class="search-result__title">В 2017 году зарегистрированы ТМ «<font>Сытный</font>» и ТМ «<font>Сытный</font> ПРО». С 2019 года разработаны и выведены на рынок корма ТМ «КОРМЛИНА».</div>
+                        <div class="search-result__description">В производстве используется только качественное сырье. Часть зерна выращивается по техническому заданию нашей производственной лаборатории сельскохозяйственными производителями, практикующими безопасное земледелие по системе «Био».</div>
+                        <div class="search-item__section">Новости</div>
+                    </div>
+                </div>
+                <div class="result-item row">
+                    <div class="col-1"></div>
+                    <div class="col-11">
+                        <div class="search-result__title"><font>Сытный</font>™ PRO Телёнок рост</div>
+                        <div class="search-result__description"></div>
+                        <div class="search-item__section">Каталог товаров</div>
+                    </div>
+                </div>
+                <div class="result-item row">
+                    <div class="col-1"></div>
+                    <div class="col-11">
+                        <div class="search-result__title"><font>Сытный</font>™ PRO Телёнок рост</div>
+                        <div class="search-result__description"></div>
+                        <div class="search-item__section">Каталог товаров</div>
+                    </div>
+                </div>
+            </div>
+            <div class="custom-pagination row no-gutters justify-content-center"">
+                <nav class="col-10" aria-label="...">
+                    <ul class="pagination pagination-lg">
+                        <li class="page-item disabled">
+                            <a class="page-link" href="#" tabindex="-1">1</a>
+                        </li>
+                        <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </section>
+</div>
+
+<? require (__DIR__  . '/../footer.php'); ?>
+
+</body>
+</html>
