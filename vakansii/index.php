6584c86892b1cc094b7f66f6aeb50ab675645992
itcmedia <support@itc-media.ru>
2019-09-17 17:37:59 +0300

    Add files via upload

--- vakansii/index.php
+++ vakansii/index.php
@@ -1,1 +1,296 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Вaкансии</title>
 
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="../css/style.css" rel="stylesheet"/>
+    <link href="../css/font-awesome/css/all.min.css" rel="stylesheet">
+    <link href="../css/font-awesome/css/fontawesome.min.css" rel="stylesheet">
+</head>
+<body>
+<? require (__DIR__  . '/../header.php'); ?>
+
+    <section class="breadcrumb">
+        <div class="container">
+            <ul itemscope itemtype="http://schema.org/BreadcrumbList">
+                <li class="breadcrumb-item" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+                    <a itemscope itemtype="http://schema.org/Thing" itemprop="item" href="">
+                        <span itemprop="name">Главная</span>
+                    </a>
+                    <meta itemprop="position" content="1"/>
+                </li>
+                <li class="breadcrumb-item" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+                    <span itemprop="name">Вакансии</span>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="content">
+        <div class="container">
+            <div class="row no-gutters">
+                <div class="col-md-9">
+                    <h1>Работа у нас</h1>
+                </div>
+                <div class="col-md-3">
+                    <div class="have-questions">Не нашли нужной <a href="#">вакансии?</a></div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="container vakansii">
+            <div class="row ">
+                <div class="col-md-8">
+                    <div class="text-block">
+                        ООО «Агро-Эксперт» — это стабильно работающее и развивающееся предприятие, оснащенное современным оборудованием, компьютерной техникой и располагающее
+                        мощной
+                        производственной базой. Командой высококвалифицированных специалистов обеспечивающих надежность и профессиональный подход. Присоединяйтесь к профессионалам!
+                    </div>
+                    <p class="inner-block-name">Открытые вакансии:</p>
+                    <div class="accordion vakansii" id="accordionVakansii">
+                        <div class="card">
+                            <div class="card-header" id="heading-1">
+                                <h5 class="mb-0">
+                                    <button class="fa btn btn-link" type="button" data-toggle="collapse" data-target="#collapse-1" aria-expanded="false" aria-controls="collapse-1">
+                                        <i>1</i>
+                                        Менеджер по оптовым продажам
+                                    </button>
+                                </h5>
+                                <div class="card-description">
+                                    до 120 000 руб. в месяц
+                                </div>
+                            </div>
+                            <div id="collapse-1" class="fa collapse" aria-labelledby="heading-1" data-parent="#accordionVakansii">
+                                <div class="card-body">
+                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat
+                                    skateboard
+                                    dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee
+                                    nulla
+                                    assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
+                                    butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
+                                    labore sustainable VHS.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="heading-2">
+                                <h5 class="mb-0">
+                                    <button class="fa btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse-2" aria-expanded="false"
+                                            aria-controls="collapse-2">
+                                        <i>2</i>
+                                        PR-менеджер
+                                    </button>
+                                </h5>
+                                <div class="card-description">
+                                    то 50 000 руб. в месяц
+                                </div>
+                            </div>
+                            <div id="collapse-2" class="collapse" aria-labelledby="heading-2" data-parent="#accordionVakansii">
+                                <div class="card-body">
+                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat
+                                    skateboard
+                                    dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee
+                                    nulla
+                                    assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
+                                    butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
+                                    labore sustainable VHS.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="heading-3">
+                                <h5 class="mb-0">
+                                    <button class="fa btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse-3" aria-expanded="false"
+                                            aria-controls="collapse3">
+                                        <i>3</i>
+                                        Менеджер по работе с розничными клиентами
+                                    </button>
+                                </h5>
+                                <div class="card-description">
+                                    20 000 - 25 000 руб. в месяц
+                                </div>
+                            </div>
+                            <div id="collapse-3" class="collapse" aria-labelledby="heading-3" data-parent="#accordionVakansii">
+                                <div class="card-body">
+                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat
+                                    skateboard
+                                    dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee
+                                    nulla
+                                    assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
+                                    butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
+                                    labore sustainable VHS.
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="heading-4">
+                                <h5 class="mb-0">
+                                    <button class="fa btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse-4" aria-expanded="true"
+                                            aria-controls="collapse-4">
+                                        <i>4</i>
+                                        Директор представительства в Санкт-Петербурге
+                                    </button>
+                                </h5>
+                                <div class="card-description">
+                                    з/п по результатам собеседования
+                                </div>
+                            </div>
+                            <div id="collapse-4" class="collapse show" aria-labelledby="heading-4" data-parent="#accordionVakansii">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <p>Ожидания от кандидата:</p>
+                                        <ul>
+                                            <li>быть гостеприимным хозяином в торговом зале, бережно и мудро обращающимся с клиентами, персоналом и самим торговым залом;</li>
+                                            <li>обеспечивать выполнение плана продаж;</li>
+                                            <li>повышать качество сервиса;</li>
+                                            <li>формировать команду, привлекать, проводить отбор и адаптацию сотрудников, выстраивать систему обучения;</li>
+                                        </ul>
+                                        <p>Что для нас важно:</p>
+                                        <ul>
+                                            <li>опыт управления розничным магазином и штатом сотрудников от 30 человек;</li>
+                                            <li>успешный опыт внедрения системы обучения и наставничества;</li>
+                                            <li>опыт проведения тренингов или лекций;</li>
+                                            <li>способность создать эффективную команду;</li>
+                                            <li>готовность давать и принимать обратную связь, уметь мотивировать команду и вдохновлять на потрясающие результаты;</li>
+                                        </ul>
+                                        <p>Что даст работа в Агро-эксперт:</p>
+                                        <ul>
+                                            <li>достойное вознаграждение, соответствующее опыту и компетенциям, вкуснейшие завтраки, обеды и ужины в кафе с фермерскими
+                                                продуктами;
+                                            </li>
+                                            <li>открытым специально для сотрудников;</li>
+                                            <li>дополнительный отпуск для профессионального развития;</li>
+                                            <li>корпоративные мероприятия (1 раз в год выезд на горнолыжные склоны);</li>
+                                            <li>корпоративное обучение;</li>
+                                        </ul>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-6">
+                                            <button type="button" class="btn btn-success custom-success">Откликнуться на вакансию</button>
+                                        </div>
+                                        <div class="col-6">
+                                            <div class="vakansii-phone-text">
+                                                <p>Или позвоните по телефону:</p>
+                                                <p><a htef="tel:+7493514771:">+7 (49351) 4-73-71</a> с 8:00 до 17:30</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-header" id="heading-5">
+                                <h5 class="mb-0">
+                                    <button class="fa btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapse-5" aria-expanded="false"
+                                            aria-controls="collapseThree">
+                                        <i>5</i>
+                                        Электромеханик
+                                    </button>
+                                </h5>
+                                <div class="card-description">
+                                    45 000 руб. в месяц
+                                </div>
+                            </div>
+                            <div id="collapse-5" class="collapse" aria-labelledby="heading-5" data-parent="#accordionVakansii">
+                                <div class="card-body">
+                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat
+                                    skateboard
+                                    dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee
+                                    nulla
+                                    assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur
+                                    butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
+                                    labore sustainable VHS.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="requisites">
+                        <div class="requisites-body">
+                            <p>Отдел кадров:</p>
+                            <p>E-mail:</p>
+                            <a href="email:ob@agroexpert2007.ru">ob@agroexpert2007.ru</a>
+                            Константинопольская
+                            Эльвира Николаевна
+                            <p>Телефон:</p>
+                            7 (49351) 4-73-71
+                            <p>
+                            <span>Звоните: с 8:00 до 17:30</span>
+                            </p>
+                        </div>
+                        <div class="bottom-button">
+                            <a href="#">Все контакты завода</a>
+                        </div>
+                    </div>
+                    <div class="informer-vakansii-block">
+                        <i class="fa fa-info-circle" aria-hidden="true"></i> Официальное трудоустройство
+                        в соответствии с трудовым кодексом РФ.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+<section class="vakansii-form">
+    <div class="container">
+        <div class="row no-gutters">
+            <div class="col-md-10 offset-md-1">
+                <p>Не нашли нужной вакансии?</p>
+                <p>Однако считаете, что будете нам полезны - обязательно напишите нам!</p>
+            </div>
+        </div>
+        <div class="row no-gutters">
+            <div class="col-md-8 offset-md-2">
+                <form class="col-md-12">
+                    <div class="form-row">
+                        <label for="inputName">Представьтесь:</label>
+                        <input type="text" class="form-control" id="inputName" placeholder="Как к вам обращаться?">
+                    </div>
+                    <div class="form-row">
+                        <div class="form-group col-md-6">
+                            <label for="inputEmail4">Электронная почта:</label>
+                            <input type="email" class="form-control" id="inputEmail4" placeholder="Введите e-mail">
+                        </div>
+                        <div class="form-group col-md-6">
+                            <label for="inputPassword4">Телефон:</label>
+                            <input type="text" class="form-control is-invalid" id="inputPassword4" placeholder="+7">
+                            <div class="invalid-feedback">
+                                Пожалуйста заполните данное поле.
+                            </div>
+                        </div>
+                    </div>
+                    <div class="mb-3">
+                        <label for="validationTextarea">Сообщение</label>
+                        <textarea class="form-control" rows="6" id="validationTextarea" placeholder="Напишите, чем вы будете полезны"></textarea>
+                    </div>
+                    <div class="custom-file">
+                        <input type="file" class="custom-file-input" id="customFile">
+                        <label class="custom-file-label" for="customFile">Прикрепить резюме</label>
+                        <span>Можно прикрепить до 3-х файлов</span>
+                    </div>
+                    <div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" id="customCheck1">
+                        <label class="custom-control-label" for="customCheck1">
+                            Согласен на обработку <a href="#">персональных данных</a> и с условиями <a href="#">оферты</a>
+                        </label>
+                    </div>
+                    <div class="form-row submit">
+                        <button type="submit" class="btn btn-success">Отправить заявку</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<? require (__DIR__  . '/../footer.php'); ?>
+
+</body>
+</html>
