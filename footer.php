2292408e06f9ba2d63e45e556c5c737c11a94d58
Qwazik <qwazik21@gmail.com>
2019-09-25 10:15:12 +0300

    fix

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
         <div class="row">
             <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6">
 
-                <img src="images/logo.png" alt="KORMLINA" width="197">
+                <img src="/images/logo.png" alt="KORMLINA" width="197">
                 <p>Шуйский комбикормовый завод. Производство и продажа <br> комбикормов по всей России. <br> Все права защищены. </p>
                 <button class="bottom-menu__button no-collor_btn" popup-target="usagr-pp">Связаться с нами</button>
             </div>
@@ -116,11 +116,11 @@
     </div>
 </div>
 
-<script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
-<script type="text/javascript" src="js/jquery.fancybox.min.js"></script>
-<script type="text/javascript" src="js/bootstrap/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/parallax.min.js"></script>
-<script type="text/javascript" src="js/parallaxjs.js"></script>
-<script type="text/javascript" src="js/maskedinput.js"></script>
-<script type="text/javascript" src="plugins/owl-carousel/owl.carousel.min.js"></script>
-<script type="text/javascript" src="js/script.js"></script>
+<script type="text/javascript" src="/js/jquery-3.3.1.min.js"></script>
+<script type="text/javascript" src="/js/jquery.fancybox.min.js"></script>
+<script type="text/javascript" src="/js/bootstrap/bootstrap.min.js"></script>
+<script type="text/javascript" src="/js/parallax.min.js"></script>
+<script type="text/javascript" src="/js/parallaxjs.js"></script>
+<script type="text/javascript" src="/js/maskedinput.js"></script>
+<script type="text/javascript" src="/plugins/owl-carousel/owl.carousel.min.js"></script>
+<script type="text/javascript" src="/js/script.js"></script>
